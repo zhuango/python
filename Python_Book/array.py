bcd5943c7eee1e941b5368db4b4be997c23d38c7
LiuZhuang <og192liu@163.com>
2016-04-19 16:29:51 +0800

    randomize array elements

--- Python_Book/array.py
+++ Python_Book/array.py
@@ -1,3 +1,10 @@
-array = [1, 2, 3, 4, 5]
+# ss 
+import numpy
+
+array = [[1, 2, 3, 4, 5],
+         [6, 7, 8, 9, 10]]
 array = array[:2]
+idx = numpy.arange(len(array[0]))
+numpy.random.shuffle(idx)
+array = ([array[0][n] for n in idx])
 print(array)
