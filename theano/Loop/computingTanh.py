93aaa20562d56af132b567a6c8c083373e675411
Og192 <og192liu@gmail.com>
2016-04-20 19:30:33 +0800

    lstm

--- theano/Loop/computingTanh.py
+++ theano/Loop/computingTanh.py
@@ -2,23 +2,23 @@ import theano
 import theano.tensor as T
 import numpy as np
 import time
-#Computing tanh(x(t).dot(W) + b) elementwise
+# Computing tanh(x(t).dot(W) + b) elementwise
 
-#define the tensor variables
+# define the tensor variables
 X = T.matrix("X")
 W = T.matrix("W")
 b_sym = T.vector("b_sym")
 
 results, updates = theano.scan(lambda v: T.tanh(T.dot(v, W) + b_sym), sequences = X)
 
-compute_elementwise = theano.function(inputs = [X, W, b_sym], outputs = results)
+compute_elementwise = theano.function(inputs=[X, W, b_sym], outputs=results)
 
-#test values
-#eye Return a 2-D array with ones on the diagonal and zeros elsewhere.
-x = np.eye(2, dtype = theano.config.floatX)
-#ones Return a new array of given shape and type, filled with ones.
-w = np.ones((2, 2), dtype = theano.config.floatX)
-b = np.ones((2), dtype = theano.config.floatX)
+# test values
+# eye Return a 2-D array with ones on the diagonal and zeros elsewhere.
+x = np.eye(2, dtype=theano.config.floatX)
+# ones Return a new array of given shape and type, filled with ones.
+w = np.ones((2, 2), dtype=theano.config.floatX)
+b = np.ones(2, dtype=theano.config.floatX)
 b[1] = 2
 
 
@@ -28,7 +28,7 @@ t1 = time.time()
 print("compute_elementwise took %f sec." % (t1 - t0))
 
 t0 = time.time()
-#comparison with numpy
+# comparison with numpy
 print(np.tanh(x.dot(w) + b))
 t1 = time.time()
 print("np.tanh took %f sec." % (t1 - t0))
