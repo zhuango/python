84b7532a7c6df9fe5715ea67b0de1ea310eff34c
Og192 <og192liu@gmail.com>
2016-07-01 16:06:25 +0800

    mul class by using lr

--- machine-learning-algorithms/Ng/logisticRegression/logisticRegressionMulClass.py
+++ machine-learning-algorithms/Ng/logisticRegression/logisticRegressionMulClass.py
@@ -3,13 +3,12 @@ import pylab as pl
 import numpy as np
 import scipy
 from scipy.optimize import minimize, rosen, rosen_der
-
+import scipy.io as sio
 def loadData(filename):
-    data = []
-    with open(filename, 'r') as f:
-        for line in f:
-            data.append(list(map(float, line.split(','))))
-    return numpy.array(data)
+    data=sio.loadmat(filename)
+    X=data['X']  
+    y=data['y']
+    return (X, y)
 
 def plotData(X, Y):
     x = X[::, 0:1]
@@ -25,22 +24,6 @@ def sigmoid(z):
     result = numpy.zeros(z.shape);
     result = 1.0 / (1.0 + numpy.exp(-z));
     return result;
-
-# def costFunctionOld(theta,X, Y, lamda):
-#     m = len(Y);
-#     n = len(theta)
-#     J = 0;
-#     grad = numpy.zeros((n, 1))
-
-#     z = sigmoid(numpy.dot(X, theta));    
-
-#     J = 1.0 / m * numpy.sum(-Y * numpy.log(z) - (1 - Y) * numpy.log(1 - z)) + lamda / (2.0 * m) * numpy.sum(theta * theta)
-
-#     for i in range(n - 1):
-#         grad[i, 0] = 1.0 / m * numpy.sum((z - Y) * X[::, i:i+1]) + lamda / m * theta[i, 0]
-#     grad[n - 1, 0] = 1.0 / m * numpy.sum((z - Y) * X[::, n - 1:n])
-
-#     return (J, grad)
 
 def costFunction(thetaFormat,X, Y, lamda):
     theta = thetaFormat.reshape((len(thetaFormat), 1))
@@ -77,37 +60,45 @@ def mapFeature(X1, X2, degree):
             out[::, featureNumber:featureNumber + 1] = numpy.power(X1, i - j) * numpy.power(X2, j);
             featureNumber += 1
     return out;
+
+def formatY(Y, c):
+    formY = numpy.zeros((len(Y), 1))
+    for j in range(m):
+        if Y[j, 0] == c:
+            formY[j, 0] = 1
+        else:
+            formY[j, 0] = 0
+    return formY
 if __name__ == "__main__":
 
-    data = loadData('ex2data2.txt')
-    # for i in range(len(data)):
-    #     data[i].insert(0, 0);
-    X = numpy.array(data[::, 0:2]) # first 2 column represents X.
-    Y = numpy.array(data[::, 2:3]) # last column represents Y.
+    (X, Y) = loadData('ex3data1.mat')
 
     #plotData(X, Y)
-
-    # bias = numpy.ones((m, 1));
-    # X = numpy.append(X, bias, 1);
-    X = mapFeature(X[::,0:1], X[::, 1:2], 6)#already add the bias column.
+    m = len(X);
+    bias = numpy.ones((m, 1));
+    X = numpy.append(X, bias, 1);
+    #X = mapFeature(X[::,0:1], X[::, 1:2], 6)#already add the bias column.
     m, n = numpy.shape(X)
 
     alpha = 0.01
     lamda = 0.1
-    numberOfIter = 1
+    numLabels = 10;
     # for i in range(numberOfIter):
     #     cost, grad = costFunctionOld(theta, X, Y, lamda)
     #     theta = theta - alpha * grad;
     #     # print("cost: " + str(cost))
     #     # print("grad: " + str(grad))
-    theta = numpy.zeros((n, 1))
-    result = minimize(costFunction,theta, (X, Y, lamda),jac =logicticRegressionJac, method='CG', tol = 1e-10, options={'maxiter': 50, 'disp': True});
-    theta = result.x;
+    allTheta = numpy.zeros((numLabels, n));
+    print(X[0].shape)
+    print(allTheta.shape)
+    for i in range(numLabels):
+        theta = numpy.zeros((n, 1));
+        result = minimize(costFunction,theta, (X, formatY(Y, i + 1), lamda),jac =logicticRegressionJac, method='CG', tol = 1e-10, options={'maxiter': 50, 'disp': True});
+        allTheta[i] = result.x;
+    
     correct = 0
     for i in range(m):
-        if Y[i, 0] == 1 and numpy.dot( X[i], theta) >= 0:
+        if Y[i, 0] == numpy.argmax(sigmoid(numpy.dot(X[i], numpy.transpose(allTheta)))) + 1:
             correct += 1
-        elif Y[i, 0] == 0 and numpy.dot( X[i], theta) < 0:
-            correct += 1 
-    print(theta)
+    print(allTheta)
     print("Accuracy: " + str(float(correct) / float(m)))
