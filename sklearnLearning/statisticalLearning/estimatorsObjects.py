a24f9bd97f000c5c52868985c98b3c02580561b5
Og192 <og192liu@gmail.com>
2016-07-25 21:02:13 +0800

    fail to save

---
+++ sklearnLearning/statisticalLearning/estimatorsObjects.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+#the main API implemented by scikit-learn is that of the estimator. An estimator is any object that learns from data; it may be a classification, regression or clustering algorithm or a transformer that extracts/filters useful features from raw data.
+#All supervised estimators in scikit-learn implement a fit(X, y) method to fit the model and a predict(X) method that, given unlabeled observations X, returns the predicted labels y.
