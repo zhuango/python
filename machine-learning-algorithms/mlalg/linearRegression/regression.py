15200067eb72f052e74c208804fa9065fd5a5be5
Og192 <og192liu@gmail.com>
2017-01-21 19:19:47 +0800

    abalone dataset and ridge regression

--- machine-learning-algorithms/mlalg/linearRegression/regression.py
+++ machine-learning-algorithms/mlalg/linearRegression/regression.py
@@ -13,7 +13,7 @@ def loadDataSet(fileName):
         dataMat.append(lineArr)
         labelMat.append(float(curLine[-1]))
     return dataMat, labelMat
-
+# w = (X.T * X) ^ -1 * X.T * y
 def standRegres(xArr, yArr):
     xMat = mat(xArr)
     yMat = mat(yArr).T
@@ -24,6 +24,7 @@ def standRegres(xArr, yArr):
     ws = xTx.I * (xMat.T*yMat)
     return ws
 
+# w = (X.T * W * X) ^ -1 * X.T * W * y
 # weighted data points.
 # Gaussian kernel:
 # w(i, i) = exp(|x(i) - x| / (-2 * k^2))
@@ -50,6 +51,40 @@ def lwlrTest(testArr, xArr, yArr, k=1.0):
         yHat[i] = lwlr(testArr[i], xArr, yArr, k)
     return yHat
 
+def rssError(yArr, yHatArr):
+    return ((yArr - yHatArr) ** 2).sum()
+
+# w = (X.T * X + lambda * I) ^ -1 * X.T * y
+# Ridge regression adds an additional matrix (lambda * I) to the matrix (X.T * X) so that it’s non-singular,
+# and we can take the inverse of the whole thing: (X.T * X + lambda * I)
+# Ridge regression was originally developed to deal with the problem of having more
+# features than data points. But it can also be used to add bias into our estimations, giving us a better estimate.
+def ridgeRegres(xMat, yMat, lam = 0.2):
+    xTx = xMat.T * xMat
+    denom = xTx + eye(shape(xMat)[1]) * lam
+    if linalg.det(denom) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+
+    numTestPts = 30
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, exp(i-10))
+        wMat[i,:] = ws.T
+    return wMat
 
 xArr, yArr = loadDataSet('ex0.txt')
 print(xArr)
@@ -83,5 +118,13 @@ xSort = xMat[srtInd][:,0,:]
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(xSort[:, 1], yHat[srtInd])
-#ax.scatter(xMat[:, 1].flatten().A[0], mat(yArr).T.flatten().A[0], s = 2, c = 'red')
+ax.scatter(xMat[:, 1].flatten().A[0], mat(yArr).T.flatten().A[0], s = 2, c = 'red')
+plt.show()
+
+abX, abY = loadDataSet('abalone.txt')
+ridgeWeights = ridgeTest(abX, abY)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(ridgeWeights)
 plt.show()
