6a9fc6817acb48d61ef44cf889edf02914cb8560
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2017-12-11 10:52:52 +0800

    pca

---
+++ sklearnLearning/unsupervisedLearning/pca.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.decomposition import PCA
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+pca = PCA(n_components=2)
+pca.fit(X)
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
