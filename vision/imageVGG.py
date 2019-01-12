aff98e776380c5ef0bcf0c389b052ebbc566ad1c
zhuango <zhuangliu1992@mail.dlut.edu.cn>
2019-01-12 10:44:36 +0800

    add comments

--- vision/imageVGG.py
+++ vision/imageVGG.py
@@ -14,6 +14,7 @@ class VGG(nn.Module):
             self.model = models.vgg19(pretrained=False)
             pre = torch.load(weights)
             self.model.load_state_dict(pre)
+        # output features not predictions.
         self.vgg19 = self.model.features
         
         for param in self.vgg19.parameters():
@@ -22,13 +23,15 @@ class VGG(nn.Module):
         return self.vgg19(input)
 
 tran=transforms.Compose([
+    # fixed image size for VGG.
     transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 ])
 
-img = Image.open("./CXR3335_IM-1598-2001.png")
+# Load images.
+img = Image.open("./CXR3498_IM-1702-1001.png")
 im=tran(img)
 im.unsqueeze_(dim=0)
 print(im.shape)
