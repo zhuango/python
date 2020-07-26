378d66413113a462db67c98dd6867f9cc6fbc389
zhuango <zhuangliu19992@mail.dlut.edu.cn>
2020-07-26 19:29:19 +0800

    update exercise

--- pytorchLearning/pytroch-with-deep-learning/chapter3_tensor/tensor_storage.py
+++ pytorchLearning/pytroch-with-deep-learning/chapter3_tensor/tensor_storage.py
@@ -10,3 +10,11 @@ print(points_storage[0])
 # change a elements in storage object would effect all the tensor object that refer it.
 points_storage[0] = 100
 print(points)
+
+# view
+points_view = points.view(6, 1)
+points_view[0][0] = 4023
+print(points_view.stride())
+print(points_view.storage())
+print(points.storage())
+print(points.stride())
